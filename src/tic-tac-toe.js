1a005f5e261648a81dbefd770ea8a1be394e6b6f
Alina1989 <autoflame.by@gmail.com>
2017-02-02 09:56:35 +0200

    t

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,13 +1,23 @@
 class TicTacToe {
     constructor() {
+        this.area = [[null],[null],[null], [null],[null],[null],[null],[null],[null]];
+        this.player = 'x';
 
     }
 
     getCurrentPlayerSymbol() {
+        return this.player
 
     }
 
     nextTurn(rowIndex, columnIndex) {
+
+        if (this.area(rowIndex, columnIndex) == null)
+            return this.area(rowIndex, columnIndex) = this.player;
+        if (this.player = 'x')
+            return this.player = 'o'
+        else 
+            return this.player = 'x'
 
     }
 
