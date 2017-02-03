9b0b4a86f572b3c1eb62e51b701dd7798d4e1c5c
Alina1989 <autoflame.by@gmail.com>
2017-02-03 23:05:38 +0200

    first

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,39 +1,81 @@
 class TicTacToe {
     constructor() {
-        this.area = [[null],[null],[null], [null],[null],[null],[null],[null],[null]];
+        this.area[][] = null;
         this.player = 'x';
+        this.filledArea = 0;
 
+
+        
     }
 
     getCurrentPlayerSymbol() {
-        return this.player
+        return this.player;
 
     }
 
     nextTurn(rowIndex, columnIndex) {
-
-        if (this.area(rowIndex, columnIndex) == null)
-            return this.area(rowIndex, columnIndex) = this.player;
+        if (this.area (rowIndex, columnIndex) === null)
+            return this.area (rowIndex, columnIndex) = this.player;
         if (this.player = 'x')
             return this.player = 'o'
         else 
-            return this.player = 'x'
+            return this.player = 'x';
+    
 
     }
 
-    isFinished() {
+    isFinished() { if (this.getWinner())
+        else
+        return false
+          return this.isDraw();
+       
 
     }
 
-    getWinner() {
+    getWinner() {  // rows
+
+        if ( this.area[0][0] === this.area[0][1] === this.area[0][2] === 'x' || this.area[1][0] === this.area[1][1] === this.area[1][2] === 'x' || 
+        this.area[2][0] === this.area[2][1] === this.area[2][2] === 'x')
+            return this.getWinner
+        else if (this.area[0][0] === this.area[0][1] === this.area[0][2] === 'o' || this.area[1][0] === this.area[1][1] === this.area[1][2] === 'o' || 
+        this.area[2][0] === this.area[2][1] === this.area[2][2] === 'o')
+            return this.getWinner;
+
+        // columns
+
+        if ( this.area[0][0] === this.area[1][0] === this.area[2][0] === 'x' || this.area[0][1] === this.area[1][1] === this.area[2][1] === 'x' || 
+        this.area[2][0] === this.area[2][1] === this.area[2][2] === 'x')
+            return this.getWinner
+        else if (this.area[0][0] === this.area[1][0] === this.area[2][0] === 'o' || this.area[0][1] === this.area[1][1] === this.area[2][1] === 'o' || 
+        this.area[0][2] === this.area[1][2] === this.area[2][2] === 'o')
+            return this.getWinner;
+
+        // diagonals
+
+         if (this.area[0][0] === this.area[1][1] === this.area[2][2] === 'o' || this.area[2][0] === this.area[1][1] === this.area[0][2] === 'o')
+            return getWinner
+        else if (this.area[0][0] === this.area[1][1] === this.area[2][2] === 'o' || this.area[2][0] === this.area[1][1] === this.area[0][2] === 'o')
+            return this.getWinner; 
+
+
 
     }
 
-    noMoreTurns() {
+    noMoreTurns() { 
+        for (var i = 0; i <= 2; ++i) {
+               for (var j = 0; j <= 2; ++j) {
+                    if (this.area[i][j] == null) {
+                         return false;
+                    }
+               }
+          }
+
+          return true;
+
 
     }
 
-    isDraw() {
+    isDraw() { if this.noMoreTurns() === 
 
     }
 
